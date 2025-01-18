a1d09856e10c2048e4ddcded23476452c71a955d
Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>
2025-01-18 19:54:35 +0300

    initial commit
    Signed-off-by: Eduard-soft <92379627+Eduard-soft@users.noreply.github.com>

--- src/app/auth/Auth.tsx
+++ src/app/auth/Auth.tsx
@@ -1,3 +1,25 @@
+'use client'
+
+import styles from './Auth.module.scss'
+import { useState } from "react"
+import { useAuthForm } from "./useAuthForm"
+
+
 export function Auth() {
-	return <div>Auth</div>
+
+	const [isReg, setIsReg] = useState(false)
+
+	// const { onSubmit, form, isPending } = useAuthForm(isReg)
+
+	return <div className={styles.wrapper}>
+		<div className={styles.left}>
+			<Image 
+					src='/images/auth.svg'
+					alt='Auth Logo'
+					width={100}
+					height={100}
+			/>
+			</div>
+		<div className={styles.right}></div>
+	</div>
 }
